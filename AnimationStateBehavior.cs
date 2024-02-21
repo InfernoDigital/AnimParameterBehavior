1ac0ae2efab96e6a0fbd514302319e6d8883cbb7
StormyDoesVR <firestorm185@gmail.com>
2024-02-21 08:38:51 -0500

    AnimationStateBehavior added
    Uploading the script ChatGPT generated that makes it super simple to make animation parameter changes when entering an animation state.

---
+++ AnimationStateBehavior.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class AnimationStateBehavior : StateMachineBehaviour
+{
+    [System.Serializable]
+    public class AnimationAction
+    {
+        public string parameterName;
+        public ActionType actionType;
+        public float floatValue;
+        public int intValue;
+        public bool boolValue;
+
+        public enum ActionType
+        {
+            Float,
+            Int,
+            Bool,
+            Trigger
+        }
+    }
+
+    public AnimationAction[] actions;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        ApplyActions(animator);
+    }
+
+    private void ApplyActions(Animator animator)
+    {
+        if (actions != null)
+        {
+            foreach (var action in actions)
+            {
+                switch (action.actionType)
+                {
+                    case AnimationAction.ActionType.Float:
+                        animator.SetFloat(action.parameterName, action.floatValue);
+                        break;
+                    case AnimationAction.ActionType.Int:
+                        animator.SetInteger(action.parameterName, action.intValue);
+                        break;
+                    case AnimationAction.ActionType.Bool:
+                        animator.SetBool(action.parameterName, action.boolValue);
+                        break;
+                    case AnimationAction.ActionType.Trigger:
+                        animator.SetTrigger(action.parameterName);
+                        break;
+                    default:
+                        Debug.LogError("Unsupported action type");
+                        break;
+                }
+            }
+        }
+    }
+}
